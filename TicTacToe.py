1588209d42c29b48b53a6562e8d685b353bf1594
rachelcastellino <castellinorachel@gmail.com>
2023-01-09 14:11:26 -0800

    added GUI

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,7 @@
 import sys
 
-
 def main():
     playGame()
-
-# ways to make better
-# refactor to combine player functions
 
 def buildBoard():
     lst = ['0', '1', '2', '3','4', '5', '6', '7', '8']
@@ -27,76 +23,98 @@ def playGame():
     board = buildBoard()
     printBoard(board)
     turns = 1
-    player = 0
     p1 = set()
     p2 = set()
-    value = None
     check = 'Continue'
 
-    if turns >= 3 and turns < 8:
-        check = checkWinner(p1, p2)
-
-    while turns < 8:
-        if turns >= 6:
-            check = checkWinner(p1, p2, board)
+    while turns <= 8:
+        if len(p1) > 2 or len(p2) > 2:
+            check = checkWinner(p1, p2)
             if check != 'Continue':
-                return check
-
-        if turns % 2 != 0 or turns == 1:
-            if value is None:
-                value = int(input("Player 1, enter a number between 0 to 8: "))
-
-            if value > 8 or value < 0:
-                value = int(input("You entered an invalid number, enter a number between 0 to 8: "))
-            else:
-                if board[value] not in ['X', 'O']:
-                    p1.add(value)
-                    board[value] = 'X'
-                    turns += 1
-                    value = None
-                    printBoard(board)
-                else:
-                    value = int(input("The cell you picked is already filled, please pick a new number: "))
-
-        else:
-            if value is None:
-                value = int(input("Player 2, enter a number between 0 to 8: "))
-            if value > 8 or value < 0:
-                value = int(input("You entered an invalid number, enter a number between 0 to 8: "))
-            else:
-                if board[value] not in ['X', 'O']:
-                    p2.add(value)
-                    board[value] = 'O'
-                    turns += 1
-                    value = None
-                    printBoard(board)
-                else:
-                    value = int(input("The cell you picked is already filled, please pick a new number: "))
+                print(check)
+                break
+        turns, board, p1, p2 = player(turns, board, p1, p2)
     else:
-        check = "The game is a tie!"
-        print(check)
+        print("The game is a tie!")
 
-    return check
+def player(turns, board, p1, p2):
+    print('turns', turns)
+    if turns % 2 != 0 or turns == 1:
+        player = 'X'
+        value = int(input("Player X, enter a number between 0 to 8: "))
+        if value > 8 or value < 0:
+            value = int(input("You entered an invalid number, enter a number between 0 to 8: "))
+        elif board[value] in ['X', 'O']:
+            value = int(input("The cell you picked is already filled, please pick a new number: "))
+        p1.add(value)
+    else: 
+        player = 'O'
+        value = computerPlayer(p1, p2, board)
+        p2.add(value)
+        print('Player O played {0} value'.format(value))
 
-def checkWinner(p1, p2, board):
+    board[value] = player
+    print('p1', p1)
+    print('p2', p2)
+    turns += 1
+    value = None
+    printBoard(board)
+
+    return turns, board, p1, p2
+
+def computerPlayer(p1, p2, board):
+    # computer player is always O
+    # return the value that it will play
+    px = p1.copy()
+    po = p2.copy()
+    # if computer player wins on next game play that one
+    print(id(px) == id(p1))
+    for value in range(len(board)):
+        if board[value] not in ['X', 'O']:
+            po.add(value)
+            if len(po) >= 3:
+                check = checkWinner(px, po)
+                if check == "!!! Player 2 won the game !!!":
+                    return value
+            else:
+                po.remove(value)
+    # else block X
+    for value in range(len(board)):
+        if board[value] not in ['X', 'O']:
+            px.add(value)
+            if len(px) >= 3:
+                check = checkWinner(px, po)
+                if check == "!!! Player 1 won the game !!!":
+                    return value
+            else:
+                px.remove(value)
+    # else fill the middle
+    if board[4] == '4':
+        return 4
+    else:
+        for value in range(len(board)):
+            if board[value] not in ['X', 'O']:
+                return value
+
+    
+
+def checkWinner(p1, p2):
     pattern = 0
-    output = None
+    check = None
     patterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    while output is None and pattern <8:
+    while not check and pattern < 8:
         if set(patterns[pattern]).issubset(p1):
-            output = "Player 1 won the game"
-            break
+            check = "!!! Player 1 won the game !!!"
+            return check
         if set(patterns[pattern]).issubset(p2):
-            output = "Player 2 won the game"
-            break
+            check = "!!! Player 2 won the game !!!"
+            return check
+
         pattern += 1
 
-    if output is None:
-        output = "Continue"
-    else:
-        print(output)
-        printBoard(board)
-    return output
+    if not check:
+        check = "Continue"
+        return check
 
 
 if __name__ == "__main__":
